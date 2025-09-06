07c443d8707e3f1ce65e63ecfc1363fe975b0fce
kertorinfo-lab <kertor.info@gmail.com>
2025-09-06 11:49:16 +0200

    Create nationalities.js

---
+++ nationalities.js
@@ -0,0 +1,12 @@
+// Nemzetiség adatok
+const NATIONALITIES = {
+    'hu': { name: 'Magyar', flag: 'https://flagcdn.com/w40/hu.png' },
+    'gb': { name: 'Angol', flag: 'https://flagcdn.com/w40/gb.png' },
+    'es': { name: 'Spanyol', flag: 'https://flagcdn.com/w40/es.png' },
+    'de': { name: 'Német', flag: 'https://flagcdn.com/w40/de.png' },
+    'fr': { name: 'Francia', flag: 'https://flagcdn.com/w40/fr.png' },
+    'it': { name: 'Olasz', flag: 'https://flagcdn.com/w40/it.png' },
+    'br': { name: 'Brazil', flag: 'https://flagcdn.com/w40/br.png' },
+    'ar': { name: 'Argentin', flag: 'https://flagcdn.com/w40/ar.png' },
+    'pt': { name: 'Portugál', flag: 'https://flagcdn.com/w40/pt.png' }
+};
